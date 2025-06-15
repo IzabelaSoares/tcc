df6d8c5a5210053f4ae1f443ff48585187444cfd
IzabelaSoares <soares.izabelamaria@gmail.com>
2025-06-15 11:41:47 -0300

    Removendo o header do stack navigator

--- frontend/src/components/login/ForgotPassword.tsx
+++ frontend/src/components/login/ForgotPassword.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 export default function ForgotPassword() {
   return (
     <View className="flex-row justify-center">
-      <Text className={`text-base font-bold ml-1 text-white`}>
+      <Text className={`text-base font-bold ml-1 text-white mt-4`}>
         Esqueci minha senha
       </Text>
     </View>
--- frontend/src/navigation/AuthStack.tsx
+++ frontend/src/navigation/AuthStack.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Login, SignUp } from '../screens';
+import { Home, Login, SignUp } from '../screens';
 
 type AuthStackParamList = {
   Login: undefined;
   SignUp: undefined;
+  Home: undefined
 };
 
 const Stack = createNativeStackNavigator<AuthStackParamList>();
 
 export default function AuthStack() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="SignUp" component={SignUp} />
     </Stack.Navigator>
