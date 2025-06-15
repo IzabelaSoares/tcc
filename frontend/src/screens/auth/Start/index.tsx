9b8431ea10d7537f39c4117566038eea116ad9d9
IzabelaSoares <soares.izabelamaria@gmail.com>
2025-06-15 18:05:47 -0300

    Renomeado componente Home para Start

--- frontend/src/screens/auth/Start/index.tsx
+++ frontend/src/screens/auth/Start/index.tsx
@@ -1,9 +1,9 @@
 import { View } from "react-native";
 import { LogoTitle, TagLine } from "../../../components/logo";
 import { ForgotPassword, LoginLink } from "../../../components/login";
-import { Illustration, CreateAccountButton } from "../../../components/home";
+import { Illustration, CreateAccountButton } from "../../../components/start";
 
-export default function Home() {
+export default function Start() {
   return (
     <View className="flex-1 justify-center items-center bg-primary px-6">
       <LogoTitle textColor="text-white" />
--- frontend/src/screens/authenticated/Home/index.tsx
+++ frontend/src/screens/authenticated/Home/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { LogoTitle } from "../../../components/logo";
 
 export default function Home() {
   return (
-    <View>
-      <Text>Home autenticado</Text>
+    <View className="flex-1 justify-center items-center bg-primary px-6" >
+      <LogoTitle textColor="text-white" />
     </View>
   );
 }
